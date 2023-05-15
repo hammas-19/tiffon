b34e5dd2ce8af402f67bf1ce8fd30d0370abd92e
hammas-19 <hammasmasood@gmail.com>
2023-05-15 19:32:24 +0500

    tested a component

---
+++ components/complimenmtry.vue
@@ -0,0 +1,13 @@
+<template>
+    <div class="max-w-7xl mx-auto  px-3 flex justify-between items-end border-b md:pb-5 pb-3">
+
+        <div class="flex flex-col text-black">
+            <span class="md:text-xl text-sm font-headings">Men's</span>
+            <h1 class="md:text-4xl text-xl font-titles">New Arrivals</h1>
+        </div>
+        <NuxtLink class="md:text-xl text-sm ">
+            View All
+        </NuxtLink>
+
+    </div>
+</template>
